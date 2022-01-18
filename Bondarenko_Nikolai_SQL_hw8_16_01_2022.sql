80b4ecf0c39c2c6721279e0e53c0c1a8fef9bd7c
Nikolay_Bondarenko <Nikolayb33@MacBook-Air-mac.local>
2022-01-18 22:21:59 +0300

    change file hw8

--- Bondarenko_Nikolai_SQL_hw8_16_01_2022.sql
+++ Bondarenko_Nikolai_SQL_hw8_16_01_2022.sql
@@ -1,24 +1,22 @@
 -- 1) пусть задан пользователь, из всех друзей найдите, кто больше всего с ним общался
 
-SELECT m.from_user_id, count(m.from_user_id) as cnt 
+SELECT m.from_user_id, count(m.from_user_id) as cnt, m.to_user_id 
 FROM messages m 
-join users u on u.id = m.from_user_id
-join friend_requests fr on u.id = fr.initiator_user_id OR u.id = fr.target_user_id
-where u.id IN ( 
- SELECT initiator_user_id FROM friend_requests WHERE (target_user_id = 1) AND status='approved' -- ИД друзей, заявку которых я подтвердил
+ join friend_requests fr on m.from_user_id IN ( 
+ SELECT fr.initiator_user_id FROM friend_requests WHERE (fr.target_user_id = 1) AND fr.status='approved' -- ИД друзей, заявку которых я подтвердил
  union
- SELECT target_user_id FROM friend_requests WHERE (initiator_user_id = 1) AND status='approved' -- ИД друзей, подтвердивших мою заявку
-) and to_user_id = 1
-group by m.from_user_id 
-order by cnt desc
-limit 1;
+ SELECT fr.target_user_id FROM friend_requests WHERE (fr.initiator_user_id = 1) AND fr.status='approved' -- ИД друзей, подтвердивших мою заявку
+) and m.to_user_id = 1 
+group by m.from_user_id, m.to_user_id 
+order by cnt desc;
 
 -- 2) подсчитайте общее количество лайков, которые получили пользователи младше 11 лет
 
 select
 count(l.id) as younger_11
-from likes l 
-join users u on l.user_id = u.id 
+from likes l
+JOIN media m ON m.id = l.media_id 
+join users u on u.id = m.user_id 
 join profiles p on p.user_id = u.id and timestampdiff(YEAR, birthday, NOW()) < 11;
 
 -- 3) посчитать кто больше всего поставил лайков, мужчины или женщины
